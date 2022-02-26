3f778f200d45450f57c6d3f2aefef57fa2893acb
Sam Odum <sam.odum1@gmail.com>
2022-02-26 23:43:43 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./data";
+function SectionTitle({ role, spec, employer, startDate, endDate }) {
+  return (
+    <div className="experience-heading">
+      <p className="role">
+        <span>{role}</span>
+        {spec && <span className="org">{spec},</span>}
+        <span className={spec ? "org bold" : "org"}>{employer}</span>
+      </p>
+      <p className="dates">
+        <span className="start">{startDate}</span> -{" "}
+        <span className="end">{endDate}</span>
+      </p>
+    </div>
+  );
+}
+function Section({ title, children }) {
+  return (
+    <div className="section">
+      <div className="section-heading">
+        <h2 className="section-title">{title}</h2>
+      </div>
+      <div className="section-children">{children}</div>
+    </div>
+  );
+}
 
 function App() {
+  const { name, contact, objective, workExperience, education, skills } = data;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="App">
+      <header className="header section-block">
+        <h1 className="name">{name}</h1>
+        <div className="contact">
+          <p>
+            <span className="contact-key">LinkedIn:</span>
+            {contact.linkedin}
+          </p>
+        </div>
+        <div className="contact">
+          <p>
+            <span className="contact-key">Email:</span>
+            {contact.email}
+          </p>
+        </div>
+        <div className="contact">
+          <p>
+            <span className="contact-key">Github:</span>
+            {contact.github}
+          </p>
+        </div>
       </header>
-    </div>
+      <section className="section-block">
+        <Section title="OBJECTIVE">
+          <div>{objective}</div>
+        </Section>
+      </section>
+      <section className="section-block">
+        <Section title="WORK EXPERIENCE">
+          <SectionTitle
+            role={workExperience.role}
+            employer={workExperience.employer}
+            startDate={workExperience.startDate}
+            endDate={workExperience.endDate}
+          />
+          <div>
+            <ul className="duty-list">
+              {workExperience.responsibilities.map((line, i) => (
+                <li key={i}>{line}</li>
+              ))}
+            </ul>
+          </div>
+        </Section>
+      </section>
+      <section className="section-block">
+        <Section title="EDUCATION">
+          {education.map((item, i) => (
+            <SectionTitle
+              role={item.certification}
+              spec={item.discipline}
+              employer={item.institution}
+              startDate={item.startDate}
+              endDate={item.endDate}
+              key={i}
+            />
+          ))}
+        </Section>
+      </section>
+      <section className="section-block">
+        <Section title="SKILLS">
+          <div>{skills.join(", ")}</div>
+        </Section>
+      </section>
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,61 @@
+const data = {
+  name: "Patrick Edet",
+  contact: {
+    linkedin: "https://linkedin.com/in/PatrickEdet",
+    email: "grandpato001@gmail.com",
+    github: "https://github.com/PatrickEdet",
+  },
+  objective:
+    "An innovative Web Developer driven with a zeal to use technology to solve problems. Known for delivering results efficiently and effectively, having exceptional interpersonal and communication skills supported by problem resolution and team work aptitude.",
+  workExperience: {
+    employer: "Aurora Loreli Limited",
+    role: "Frontend Developer",
+    startDate: "01/11/19",
+    endDate: "Present",
+    responsibilities: [
+      "Develop functional and appealing web-and mobile-based applications based on usability",
+      "Build e-commerce application and apps for deployment",
+      "Mentor new developers on the team",
+      "Provide web site maintenance and enhancements",
+      "Collaborate with team members on GitHub during projects",
+      "Use a combination of markup languages to write web pages",
+      "Write functional requirement documents and specifications",
+      "Create quality mockups and prototypes on tight time lines",
+      "Assist back-end developers with coding and troubleshooting",
+      "Create cascading stylesheets (CSS) that are consistent across all browsers and platforms",
+      "Maintain graphic standards and branding throughout product’s interface",
+      "Stay up-to-date on emerging technologies",
+      "Promote usability best practices",
+      "Assist back-end developers with coding and troubleshooting",
+    ],
+  },
+  education: [
+    {
+      institution: "University of Calabar",
+      discipline: "Botany",
+      certification: "Bachelor of Science",
+      startDate: "01/09/06",
+      endDate: "31/08/11",
+    },
+    {
+      institution: "Google",
+      discipline: "Mobile Web Development",
+      certification: "Certificate",
+      startDate: "01/03/20",
+      endDate: "31/11/20",
+    },
+  ],
+  skills: [
+    "JavaScript",
+    "HML5",
+    "CSS3",
+    "SASS",
+    "Bootstrap",
+    "Typescript",
+    "Nodejs",
+    "React",
+    "and GitHub",
+  ],
+};
+
+export default data;
